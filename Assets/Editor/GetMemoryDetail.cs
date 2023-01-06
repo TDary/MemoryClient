0a0b385d8ae1939a845aea794d0d32e6d0f9ddc0
chenDary <10444087+chendary@user.noreply.gitee.com>
2023-01-06 17:56:27 +0800

    更新更新

--- Assets/Editor/GetMemoryDetail.cs
+++ Assets/Editor/GetMemoryDetail.cs
@@ -9,157 +9,182 @@ using System;
 using UnityEditorInternal;
 using System.Threading;
 
-namespace ProfileMemory
+public class ExtractMemoryInfo : EditorWindow
 {
-  
-    public class ExtractMemoryInfo:EditorWindow
+    private string connectIP = string.Empty;
+    private static ExtractMemoryInfo Instance;
+    [MenuItem("Tool/GetMemoryDetailed")]
+    public static void GetMemoryDetails()
     {
-        private string connectIP = string.Empty;
-        private static ExtractMemoryInfo Instance;
-        [MenuItem("Tool/GetMemoryDetailed")]
-        public static void GetMemoryDetails()
+        if (Instance == null)
         {
-            if (Instance == null)
-            {
-                Instance = ScriptableObject.CreateInstance<ExtractMemoryInfo>();
-            }
-            Instance.Show();
+            Instance = ScriptableObject.CreateInstance<ExtractMemoryInfo>();
         }
-        private void OnGUI()
+        Instance.Show();
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label("请输入要连接的IP：");
+        connectIP = EditorGUILayout.TextField(connectIP);
+        if (GUILayout.Button("连接游戏"))
         {
-            GUILayout.Label("请输入要连接的IP：");
-            connectIP = EditorGUILayout.TextField(connectIP);
-            if (GUILayout.Button("连接游戏"))
+            if (!string.IsNullOrEmpty(connectIP))
             {
-                if (!string.IsNullOrEmpty(connectIP))
-                {
-                    ConnectGamesAndOpenProfiler(connectIP);
-                }
-            }
-            if(GUILayout.Button("执行一次Take Sample"))
-            {
-                TakeSimple();
-            }
-            if (GUILayout.Button("获取一次内存Detail数据"))
-            {
-                ExtractMemoryDetailed();
+                ConnectGamesAndOpenProfiler(connectIP);
             }
         }
+        if (GUILayout.Button("执行一次Take Sample"))
+        {
+            TakeSimple();
+        }
+        if (GUILayout.Button("获取一次内存Detail数据"))
+        {
+            ExtractMemoryDetailed();
+        }
+    }
 
-        public static void ConnectGamesAndOpenProfiler(string ip)
-        {
-            //打开Profiler窗口
-            EditorApplication.ExecuteMenuItem("Window/Analysis/Profiler");
-            ProfilerDriver.DirectIPConnect(ip);   //连接机器ip，需开启游戏
-        }
+    public static void ConnectGamesAndOpenProfiler(string ip)
+    {
+        //打开Profiler窗口
+        EditorApplication.ExecuteMenuItem("Window/Analysis/Profiler");
+        ProfilerDriver.DirectIPConnect(ip);   //连接机器ip，需开启游戏
+    }
 
-        public static void TakeSimple()
-        {
-            var ProfilerWindow = typeof(EditorWindow).Assembly.GetType("UnityEditor.ProfilerWindow");
-            var MemoryProfilerModule = typeof(EditorWindow).Assembly.GetType("UnityEditorInternal.Profiling.MemoryProfilerModule");
+    public static void TakeSimple()
+    {
+        var ProfilerWindow = typeof(EditorWindow).Assembly.GetType("UnityEditor.ProfilerWindow");
+        var MemoryProfilerModule = typeof(EditorWindow).Assembly.GetType("UnityEditorInternal.Profiling.MemoryProfilerModule");
 #if UNITY_2020_1_OR_NEWER
-            var ProfilerWindows = ProfilerWindow.GetField("s_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
+        var ProfilerWindows = ProfilerWindow.GetField("s_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
 #else
             var ProfilerWindows = ProfilerWindow.GetField("m_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
 #endif
-            if (ProfilerWindows.Count == 0)
-            {
-                Debug.LogError("Profile窗口未打开");
-                return;
-            }
-            var GetProfilerModule = ProfilerWindows[0].GetType().GetMethod("GetProfilerModule", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(MemoryProfilerModule);
-            var memoryModule = GetProfilerModule.Invoke(ProfilerWindows[0], new System.Object[] { ProfilerArea.Memory });
-            //截取一帧，Take Sample Editor
-            var RefreshMemoryData = memoryModule.GetType().GetMethod("RefreshMemoryData", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
-            RefreshMemoryData.Invoke(memoryModule, new System.Object[] { });
-        }
-        public static void ExtractMemoryDetailed()
+        if (ProfilerWindows.Count == 0)
         {
-            var ProfilerWindow = typeof(EditorWindow).Assembly.GetType("UnityEditor.ProfilerWindow");
-            var MemoryProfilerModule = typeof(EditorWindow).Assembly.GetType("UnityEditorInternal.Profiling.MemoryProfilerModule");
+            Debug.LogError("Profile窗口未打开");
+            return;
+        }
+        var GetProfilerModule = ProfilerWindows[0].GetType().GetMethod("GetProfilerModule", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(MemoryProfilerModule);
+        var memoryModule = GetProfilerModule.Invoke(ProfilerWindows[0], new System.Object[] { ProfilerArea.Memory });
+        //截取一帧，Take Sample Editor
+        var RefreshMemoryData = memoryModule.GetType().GetMethod("RefreshMemoryData", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+        RefreshMemoryData.Invoke(memoryModule, new System.Object[] { });
+    }
+    public static void ExtractMemoryDetailed()
+    {
+        var ProfilerWindow = typeof(EditorWindow).Assembly.GetType("UnityEditor.ProfilerWindow");
+        var MemoryProfilerModule = typeof(EditorWindow).Assembly.GetType("UnityEditorInternal.Profiling.MemoryProfilerModule");
 #if UNITY_2020_1_OR_NEWER
-            var ProfilerWindows = ProfilerWindow.GetField("s_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
+        var ProfilerWindows = ProfilerWindow.GetField("s_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
 #else
             var ProfilerWindows = ProfilerWindow.GetField("m_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
 #endif
-            var GetProfilerModule = ProfilerWindows[0].GetType().GetMethod("GetProfilerModule", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(MemoryProfilerModule);
-            var memoryModule = GetProfilerModule.Invoke(ProfilerWindows[0], new System.Object[] { ProfilerArea.Memory });
-            //内存数据在m_MemoryListView里面
-            var m_MemoryListView = MemoryProfilerModule.GetField("m_MemoryListView", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance).GetValue(memoryModule);
-            var m_Root = m_MemoryListView.GetType().GetField("m_Root", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(m_MemoryListView);
+        var GetProfilerModule = ProfilerWindows[0].GetType().GetMethod("GetProfilerModule", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(MemoryProfilerModule);
+        var memoryModule = GetProfilerModule.Invoke(ProfilerWindows[0], new System.Object[] { ProfilerArea.Memory });
+        //内存数据在m_MemoryListView里面
+        var m_MemoryListView = MemoryProfilerModule.GetField("m_MemoryListView", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance).GetValue(memoryModule);
+        var m_Root = m_MemoryListView.GetType().GetField("m_Root", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(m_MemoryListView);
 
-            MemoryElement data = MemoryElement.Create(m_Root, -1);
-            string dirName = "MemoryDetailed";
-            string fileName = string.Format("MemoryDetailed{0:yyyy_MM_dd_HH_mm_ss}.txt", DateTime.Now);
-            string outputPath = string.Format("{0}/{1}/{2}", System.Environment.CurrentDirectory, dirName, fileName);
-            string dir = Path.GetDirectoryName(outputPath);
-            if (!Directory.Exists(dir))
-            {
-                Directory.CreateDirectory(dir);
-            }
-            StreamWriter writer = new StreamWriter(outputPath);
-            WriteMemoryDetail(writer, data);
-            writer.Flush();
-            writer.Close();
-            Debug.Log(string.Format("提取Profile内存数据完成（{0}）", outputPath));
-        }
-
-        public static void WriteMemoryDetail(StreamWriter writer, MemoryElement root)
+        MemoryElement data = MemoryElement.Create(m_Root, -1);
+        string dirName = "MemoryDetailed";
+        string fileName = string.Format("MemoryDetailed{0:yyyy_MM_dd_HH_mm_ss}.txt", DateTime.Now);
+        string outputPath = string.Format("{0}/{1}/{2}", System.Environment.CurrentDirectory, dirName, fileName);
+        string dir = Path.GetDirectoryName(outputPath);
+        if (!Directory.Exists(dir))
         {
-            if (root == null) return;
-            writer.WriteLine(root.ToString());
-            foreach (var memoryElement in root.children)
+            Directory.CreateDirectory(dir);
+        }
+        StreamWriter writer = new StreamWriter(outputPath);
+        WriteMemoryDetail(writer, data);
+        writer.Flush();
+        writer.Close();
+        Debug.Log(string.Format("提取Profile内存数据完成（{0}）", outputPath));
+    }
+    public static void ExtractMemoryDetailed2(string fileName)
+    {
+        var ProfilerWindow = typeof(EditorWindow).Assembly.GetType("UnityEditor.ProfilerWindow");
+        var MemoryProfilerModule = typeof(EditorWindow).Assembly.GetType("UnityEditorInternal.Profiling.MemoryProfilerModule");
+#if UNITY_2020_1_OR_NEWER
+        var ProfilerWindows = ProfilerWindow.GetField("s_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
+#else
+            var ProfilerWindows = ProfilerWindow.GetField("m_ProfilerWindows", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(null) as IList;
+#endif
+        var GetProfilerModule = ProfilerWindows[0].GetType().GetMethod("GetProfilerModule", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(MemoryProfilerModule);
+        var memoryModule = GetProfilerModule.Invoke(ProfilerWindows[0], new System.Object[] { ProfilerArea.Memory });
+        //内存数据在m_MemoryListView里面
+        var m_MemoryListView = MemoryProfilerModule.GetField("m_MemoryListView", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance).GetValue(memoryModule);
+        var m_Root = m_MemoryListView.GetType().GetField("m_Root", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField).GetValue(m_MemoryListView);
+
+        MemoryElement data = MemoryElement.Create(m_Root, -1);
+        string dirName = "MemoryDetailed";
+        string outputPath = string.Format("{0}/{1}/{2}", System.Environment.CurrentDirectory, dirName, fileName + ".txt");
+        string dir = Path.GetDirectoryName(outputPath);
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        StreamWriter writer = new StreamWriter(outputPath);
+        WriteMemoryDetail(writer, data);
+        writer.Flush();
+        writer.Close();
+        Debug.Log(string.Format("提取Profile内存数据完成（{0}）", outputPath));
+    }
+
+    public static void WriteMemoryDetail(StreamWriter writer, MemoryElement root)
+    {
+        if (root == null) return;
+        writer.WriteLine(root.ToString());
+        foreach (var memoryElement in root.children)
+        {
+            if (memoryElement != null)
             {
-                if (memoryElement != null)
-                {
-                    WriteMemoryDetail(writer, memoryElement);
-                }
+                WriteMemoryDetail(writer, memoryElement);
             }
         }
     }
-    public class MemoryElement
+}
+public class MemoryElement
+{
+    private int depth;
+    public string name;
+    public int totalChildCount;
+    public long totalMemory;
+    public List<MemoryElement> children = new List<MemoryElement>();
+    public static MemoryElement Create(object root, int depth)
     {
-        private int depth;
-        public string name;
-        public int totalChildCount;
-        public long totalMemory;
-        public List<MemoryElement> children = new List<MemoryElement>();
-        public static MemoryElement Create(object root, int depth)
+        if (root == null) return null;
+        MemoryElement memoryElement = new MemoryElement { depth = depth };
+        memoryElement.name = (string)root.GetType().GetField("name", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root);
+        memoryElement.totalMemory = (long)root.GetType().GetField("totalMemory", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root);
+        memoryElement.totalChildCount = (int)root.GetType().GetField("totalChildCount", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root);
+        var children = root.GetType().GetField("children", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root) as IList;
+        if (children != null)
         {
-            if (root == null) return null;
-            MemoryElement memoryElement = new MemoryElement { depth = depth };
-            memoryElement.name = (string)root.GetType().GetField("name", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root);
-            memoryElement.totalMemory = (long)root.GetType().GetField("totalMemory", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root);
-            memoryElement.totalChildCount = (int)root.GetType().GetField("totalChildCount", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root);
-            var children = root.GetType().GetField("children", BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetField).GetValue(root) as IList;
-            if (children != null)
+            foreach (var child in children)
             {
-                foreach (var child in children)
-                {
-                    if (child == null) continue;
-                    //递归遍历
-                    MemoryElement t_memoryElement = Create(child, depth + 1);
-                    memoryElement.children.Add(t_memoryElement);
-                }
+                if (child == null) continue;
+                //递归遍历
+                MemoryElement t_memoryElement = Create(child, depth + 1);
+                memoryElement.children.Add(t_memoryElement);
             }
-            return memoryElement;
         }
-        public override string ToString()
+        return memoryElement;
+    }
+    public override string ToString()
+    {
+        if (depth < 0)
         {
-            if (depth < 0)
-            {
-                return string.Format("totalMemory:{0}B", totalMemory);
-            }
-            if (children.Count > 0)
-            {
-                string resultString = string.Format(new string('\t', depth) + "{0},({1}),{2}B", name, totalChildCount, totalMemory);
-                return resultString;
-            }
-            else
-            {
-                string resultString = string.Format(new string('\t', depth) + "{0},{1}B", name, totalMemory);
-                return resultString;
-            }
+            return string.Format("totalMemory:{0}B", totalMemory);
+        }
+        if (children.Count > 0)
+        {
+            string resultString = string.Format(new string('\t', depth) + "{0},({1}),{2}B", name, totalChildCount, totalMemory);
+            return resultString;
+        }
+        else
+        {
+            string resultString = string.Format(new string('\t', depth) + "{0},{1}B", name, totalMemory);
+            return resultString;
         }
     }
 }
---
+++ Assets/Scripts/HttpServer.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class HttpServer : MonoBehaviour
+{
+    public class DataMes
+    {
+        public string ip { get; set; }
+        public bool isConnected { get; set; }
+        public bool istakesimple { get; set; }
+        public string filename { get; set; }
+    }
+    private Assembly editor;
+    private System.Type type;
+    private string current_Ip = string.Empty;
+    public DataMes Message;
+    public void Start()
+    {
+        IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in ipEntry.AddressList)
+        {
+            current_Ip = ip.ToString();
+        }
+        Message = new DataMes();
+        editor = Assembly.Load("Assembly-CSharp-Editor");
+        type = editor.GetType("ExtractMemoryInfo");
+        //连接游戏
+        type.GetMethod("ConnectGamesAndOpenProfiler").Invoke(null, new object[] { Message.ip });
+        //执行TakeSample
+        type.GetMethod("TakeSimple").Invoke(null, new object[] { });
+        //执行写内存数据文件
+        type.GetMethod("ExtractMemoryDetailed2").Invoke(null, new object[] { Message.filename });
+    }
+    public void Update()
+    {
+    }
+}
